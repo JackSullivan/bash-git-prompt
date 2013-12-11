445979ed6b0a1a33c0104c774a083bf354316c4e
John Sullivan <john.t.sullivan@gmail.com>
2013-12-11 00:55:49 -0500

    Fixed bug which broke filenames with spaces.

--- gitprompt.sh
+++ gitprompt.sh
@@ -137,18 +137,6 @@ function updatePrompt() {
 
   git_prompt_config
 
-	local repo=`git rev-parse --show-toplevel 2> /dev/null`
-	if [[ ! -e "${repo}" ]]; then
-        PS1="${EMPTY_PROMPT}"
-        return
-	fi
-
-	local FETCH_HEAD="${repo}/.git/FETCH_HEAD"
-	# Fech repo if local is stale for more than $GIT_FETCH_TIMEOUT minutes
-	if [[ ! -e "${FETCH_HEAD}"  ||  -e `find ${FETCH_HEAD} -mmin +${GIT_PROMPT_FETCH_TIMEOUT}` ]]
-	then
-        git fetch --quiet
-	fi
   local -a GitStatus
   GitStatus=($("${__GIT_STATUS_CMD}" 2>/dev/null))
 
